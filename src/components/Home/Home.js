5368dfc43eb059f943784a053a3a0d1993a05c9c
Emer John Niones <nionesemerjohn@gmail.com>
2024-05-08 22:54:35 +0800

    added Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import './home.css'
+import portrait from '../../assets/portrait.png';
+import TextTransition, { presets } from 'react-text-transition';
+
+
+const ROLES = ['Software Engineer', 'DevOps Engineer', 'Materials Engineer'];
+
+export default function Home () {
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000, // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
+  
+  return (
+    <div className='home'>
+      <div className='div-home'>
+        <div className='div-home-left'>
+          <div className='introduction' id='greetings'>
+              Hello, It's Me
+          </div>
+          <div className='introduction' id='name'>
+              Emer John Niones
+          </div>
+          <div classname='introduction' id='greetings-role'>
+              And I'm a <text id='role'><TextTransition springConfig={presets.wobbly}>{ROLES[index % ROLES.length]}</TextTransition></text>
+          </div>
+          <div className='introduction' id='brief-intro'>
+            A junior software engineer fueled by passion and driven by curiosity.
+            Take a journey through my portfolio and explore how I tackle challenges 
+            to bring projects to life with flair and functionality.
+          </div>
+          <div className='div-social'>
+            <div className='div-icon'>
+              <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" viewBox="0 0 448 512"><path fill="#00aaf3" d="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"/></svg>
+              </div>
+            </div>
+            <div className='div-icon'>
+              <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" viewBox="0 0 480 512"><path fill="#00aaf3" d="M186.1 328.7c0 20.9-10.9 55.1-36.7 55.1s-36.7-34.2-36.7-55.1 10.9-55.1 36.7-55.1 36.7 34.2 36.7 55.1zM480 278.2c0 31.9-3.2 65.7-17.5 95-37.9 76.6-142.1 74.8-216.7 74.8-75.8 0-186.2 2.7-225.6-74.8-14.6-29-20.2-63.1-20.2-95 0-41.9 13.9-81.5 41.5-113.6-5.2-15.8-7.7-32.4-7.7-48.8 0-21.5 4.9-32.3 14.6-51.8 45.3 0 74.3 9 108.8 36 29-6.9 58.8-10 88.7-10 27 0 54.2 2.9 80.4 9.2 34-26.7 63-35.2 107.8-35.2 9.8 19.5 14.6 30.3 14.6 51.8 0 16.4-2.6 32.7-7.7 48.2 27.5 32.4 39 72.3 39 114.2zm-64.3 50.5c0-43.9-26.7-82.6-73.5-82.6-18.9 0-37 3.4-56 6-14.9 2.3-29.8 3.2-45.1 3.2-15.2 0-30.1-.9-45.1-3.2-18.7-2.6-37-6-56-6-46.8 0-73.5 38.7-73.5 82.6 0 87.8 80.4 101.3 150.4 101.3h48.2c70.3 0 150.6-13.4 150.6-101.3zm-82.6-55.1c-25.8 0-36.7 34.2-36.7 55.1s10.9 55.1 36.7 55.1 36.7-34.2 36.7-55.1-10.9-55.1-36.7-55.1z"/></svg>
+              </div>
+            </div>
+            <div className='div-icon'>
+              <div className='icon'>
+                <svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" viewBox="0 0 320 512"><path fill="#00aaf3" d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+              </div>
+            </div>
+            <div className='div-icon'>
+              <div className='icon'>
+              <svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" viewBox="0 0 448 512"><path fill="#00aaf3" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+              </div>
+            </div>
+          </div>
+          <div className='div-buttons'>
+            <div className='button' id='download'>
+              Download Resumé
+            </div>
+            <div className='button' id='view'>
+              View Projects
+            </div>
+          </div>
+        </div>
+        <div className='div-home-right'>
+          <img className='portrait' src={portrait}/>
+        </div>
+      </div>
+    </div>
+  )
+}
