0c4fdab9210a65f581a1effb1a9dbc658ef1766f
Emer John Niones <nionesemerjohn@gmail.com>
2024-05-08 13:42:13 +0800

    added header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./header.css"
+import logo from '../../assets/logo.png'
+
+export default function Header() {
+  return (
+    <div className='header'>
+      <img className='logo' src={logo}/>
+      <div className='menu'>
+        <div className='menu-items'>
+          Home
+        </div>
+        <div className='menu-items'>
+          About
+        </div>
+        <div className='menu-items'>
+          Projects
+        </div>
+        <div className='menu-items'>
+          Experience
+        </div>
+        <div className='menu-items'>
+          Contact
+        </div>
+      </div>
+    </div>
+  )
+}
